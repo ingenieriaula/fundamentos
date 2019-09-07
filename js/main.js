165175ba073086bcbedfe7d219d71bd8aa15007a
Kevin Antipa <kevinantipashi@gmail.com>
2019-09-07 11:52:42 -0400

    feat/creacion de contacto y empesar a usar javaScript

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// funcion de ventana modal de alerta
+// alert("hola mundo");
+// funcion(selector) metodo
+/*
+elementos a seleccionar           ETIQUETAS HTML              IDENTIFICADORES Y CLASES
+ELEMENTOS DEL DOM               ej:                             eje:
+    documento: document           nav: "nav"                        <nav class="menu"> : ".menu"
+    navegador: navigator          header: "header"                  <header id="barra"> : "#barra"
+    ventana: windows
+*/
+$(document).ready(documentoCargando);
+function documentoCargando() {
+    console.log("el documento se cargo");
+}
